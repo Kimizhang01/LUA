28f93a722aba125f3efc77bd64415fe4dcf94cea
Kimi_Zhang <1218021022@qq.com>
2019-06-02 16:01:56 +0800

    添加类

--- Run.bat
+++ Run.bat
@@ -1,1 +1,32 @@
-lua  test.lua
+@ECHO OFF
+ 
+
+:RESTART
+ 
+
+SET num=-1
+ 
+
+tasklist | find /C "LuaStudio.exe">temp.txt
+ 
+
+set /p num= < temp.txt 
+ 
+
+:: echo 找到进程数：%num%
+ 
+
+if "%num%" == "0"    (
+ 
+
+echo 重启时间是：%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2% >>RestartLog.txt
+ 
+
+start /D "C:\Program Files (x86)\LuaStudio\" LuaStudio.exe
+ 
+
+)
+
+ping -n 5 -w 1000 0.0.0.1>nul
+
+goto RESTART
--- test.lua
+++ test.lua
@@ -1,2 +1,28 @@
+--[[
 
-print 'a'
+print('a');
+print(temp);
+temp = "mystr";
+if nil then
+
+elseif true then
+
+else
+
+end
+
+
+for  do
+end
+
+while do
+end
+
+--]]
+
+tmp = 0;
+repeat
+	tmp = tmp +1;
+	print(tmp);
+until(tmp>10);
+
